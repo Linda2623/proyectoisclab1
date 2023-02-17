928d9cc0ccdd4716842df8fb2b6668bb62d0113e
linda2623 <linda.18070006@itsmotul.edu.mx>
2023-02-17 12:18:01 -0600

    primer cambio

--- proyectoisclab/src/components/HelloWorld.vue
+++ proyectoisclab/src/components/HelloWorld.vue
@@ -2,8 +2,8 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
+      Laboratorio de sistemas y aplicaciones web
+      email contacto@isclab.com.mx
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
